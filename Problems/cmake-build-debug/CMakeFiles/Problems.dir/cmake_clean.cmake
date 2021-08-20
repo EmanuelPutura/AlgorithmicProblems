513d06478b0b62138565b7ab58ba9468234cfb5a
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2021-08-20 12:58:48 +0300

    init

---
+++ Problems/cmake-build-debug/CMakeFiles/Problems.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problems.dir/main.cpp.obj"
+  "Problems.exe"
+  "Problems.exe.manifest"
+  "Problems.pdb"
+  "libProblems.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
