ae3f85ad6614e2208934f89371e1489fd4692477
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2021-10-26 10:11:34 +0300

    LeetCode Unique Paths - added two better solutions

--- Problems/Leetcode/Unique Paths.cpp
+++ Problems/Leetcode/Unique Paths.cpp
@@ -6,7 +6,8 @@
 
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
+    // Most Used Memory
+    int uniquePathsUsingNPower2ExtraSpace(int n, int m) {
         std::vector<std::vector<int>> possibilities (n);
         for (int i = 0; i < possibilities.size(); ++i)
             possibilities[i] = std::vector<int> (m, 0);
@@ -22,5 +23,31 @@ public:
                 possibilities[i][j] = possibilities[i - 1][j] + possibilities[i][j - 1];
 
         return possibilities[n - 1][m - 1];
+    }
+
+    // Less Used Memory
+    int uniquePathsUsingNTimes2ExtraSpace(int n, int m) {
+        std::vector<int> last_row (m, 1);
+        std::vector<int> current_row (m, 1);
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                current_row[j] = last_row[j] + current_row[j - 1];
+                last_row[j] = current_row[j];  // swap the two vectors
+            }
+        }
+
+        return current_row[m - 1];
+    }
+
+    // Best solution
+    int uniquePathsUsingNExtraSpace(int n, int m) {
+        std::vector<int> current_row (m, 1);
+
+        for (int i = 1; i < n; ++i)
+            for (int j = 1; j < m; ++j)
+                current_row[j] += current_row[j - 1];
+
+        return current_row[m - 1];
     }
 };
